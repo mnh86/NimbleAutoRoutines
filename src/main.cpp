a2ad808ba59538a9a1d58788812f7290d1dfb8f4
MnH <123832909+mnh86@users.noreply.github.com>
2023-01-29 22:25:34 -0500

    fix warnings and formatting

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,21 @@
 
 // Use "ESP32 Dev Module" as board
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   initNimbleSDK();
 }
 
-void loop() {
-  // Put your main code here, to run repeatedly. Do NOT use delays. 
+void loop()
+{
+  // Put your main code here, to run repeatedly. Do NOT use delays.
 
   // Modify values to be sent to the actuator.
-  
+
   // Check actuator and pendant serial ports for complete packets and update structs.
-  if (readFromPend()) { // Read values from pendant. If the function returns true, the values were updated so update the pass-through values.
+  if (readFromPend())
+  { // Read values from pendant. If the function returns true, the values were updated so update the pass-through values.
     // DEMO: Pass through data from pendant to actuator
     actuator.positionCommand = pendant.positionCommand;
     actuator.forceCommand = pendant.forceCommand;
@@ -27,17 +30,21 @@ void loop() {
 
   // This DEMO code pauses the actuator (in a very crude way) when the encoder button is pressed
   // (it will jump to whatever position the pendant is commanding at the moment the button is released)
-  if (digitalRead(ENC_BUTT)) { // Encoder button reads low when pressed.
-    driveLEDs(encoder.getCount());  // Show LEDs as demo
-    //Serial.printf("Pendant dial level: %d\n", encoder.getCount());
-  } else {
-    driveLEDs(0);   // Blank LEDs when button is pressed
-    actuator.forceCommand = 0;  // Set force command to 0 when button is pressed.
+  if (digitalRead(ENC_BUTT))
+  {                                // Encoder button reads low when pressed.
+    driveLEDs(encoder.getCount()); // Show LEDs as demo
+    // Serial.printf("Pendant dial level: %d\n", encoder.getCount());
   }
-  
-  // Send packet of values to the actuator when time is ready
-  if (checkTimer()) sendToAct();
+  else
+  {
+    driveLEDs(0);              // Blank LEDs when button is pressed
+    actuator.forceCommand = 0; // Set force command to 0 when button is pressed.
+  }
 
-  pendant.present ? ledcWrite(PEND_LED, 50) : ledcWrite(PEND_LED, 0);  // Display pendant connection status on LED.
+  // Send packet of values to the actuator when time is ready
+  if (checkTimer())
+    sendToAct();
+
+  pendant.present ? ledcWrite(PEND_LED, 50) : ledcWrite(PEND_LED, 0); // Display pendant connection status on LED.
   actuator.present ? ledcWrite(ACT_LED, 50) : ledcWrite(ACT_LED, 0);  // Display actuator connection status on LED.
 }
